47732234c7dcdc961ea14fa1149c36e09431a0b0
zorione <15937495600@163.com>
2020-09-19 14:24:30 +0800

    test

---
+++ src/components/config.js
@@ -0,0 +1,50 @@
+export const prizeList = [
+  {
+    icon: require("../assets/img/bean_500.png"), // 奖品图片
+    name: "再来一次", // 奖品名称
+	tex: "再来一次",
+    isPrize: 0 // 该奖项是否为奖品
+  },
+  {
+    icon: require("../assets/img/bean_500.png"),
+    name: "9.11",
+	tex: "火锅 炸鸡腿 牛排 意面",
+    isPrize: 1
+  },
+  {
+    icon: require("../assets/img/bean_500.png"),
+    name: "9.12",
+	tex: "红烧肉",
+    isPrize: 1
+  },
+  {
+    icon: require("../assets/img/bean_500.png"),
+    name: "9.14",
+	tex: "牛蛙 牛排",
+    isPrize: 1
+  },
+  {
+    icon: require("../assets/img/bean_500.png"),
+    name: "再来一次",
+	tex: "再来一次",
+    isPrize: 0
+  },
+  {
+    icon: require("../assets/img/bean_500.png"),
+    name: "9.17",
+	tex: "鱿鱼 海鲜",
+    isPrize: 1
+  },
+  {
+    icon: require("../assets/img/bean_500.png"),
+    name: "9.18",
+	tex: "披萨 烧仙草 烧烤 去青岛/海边吃海鲜",
+    isPrize: 1
+  },
+  {
+    icon: require("../assets/img/bean_500.png"),
+    name: "9.19",
+	tex: "烤肉 烧烤 去东北玩",
+    isPrize: 1
+  }
+]
